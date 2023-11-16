f0f8e39d22777d7042a2b14758c1ce6187166590
laughinggas <ashvni.n@gmail.com>
2023-11-16 21:35:05 +1100

    Basic done, Prop almost done

--- PadicLFunctions4/Basic.lean
+++ PadicLFunctions4/Basic.lean
@@ -1,1 +1,125 @@
-def hello := "world"
+import Mathlib.NumberTheory.DirichletCharacter.Basic
+
+open DirichletCharacter
+
+/-- If m = n are positive natural numbers, then ZMod m ≃ ZMod n. -/
+def ZMod.mul_equiv {a b : ℕ} (h : a = b) : ZMod a ≃* ZMod b := by { rw [h] }
+
+variable {R : Type} [CommMonoidWithZero R] {n : ℕ} (χ : DirichletCharacter R n)
+/-- If m = n are positive natural numbers, then their Dirichlet character spaces are the same. -/
+def equiv {a b : ℕ} (h : a = b) : DirichletCharacter R a ≃* DirichletCharacter R b := by { rw [h] }
+
+namespace DirichletCharacter
+/-- The primitive character associated to a Dirichlet character. -/
+noncomputable def reduction : DirichletCharacter R χ.conductor :=
+  Classical.choose (factorsThrough_conductor χ).choose_spec
+
+open FactorsThrough
+lemma mem_conductorSet_eq_conductor {d : ℕ} (hd : d ∈ conductorSet χ) :
+  χ.conductor ≤ (Classical.choose (hd.2 : )).conductor :=
+by
+  apply Nat.sInf_le
+  rw [mem_conductorSet_iff]
+  refine' ⟨dvd_trans (conductor_dvd_level _) hd.1, (factorsThrough_conductor (Classical.choose hd.2)).2.choose, _⟩
+  rw [changeLevel_trans _ (conductor_dvd_level _) (FactorsThrough.dvd _ hd), ←(factorsThrough_conductor (Classical.choose hd.2)).2.choose_spec]
+  apply eq_changeLevel χ hd
+
+lemma reduction_isPrimitive : isPrimitive (reduction χ) :=
+by
+  by_cases χ.conductor = 0
+  { rw [isPrimitive_def]
+    conv_rhs => rw [h]
+    rw [conductor_eq_zero_iff_level_eq_zero, h] }
+  refine le_antisymm (Nat.le_of_dvd (Nat.pos_of_ne_zero h) (conductor_dvd_level _))
+    (mem_conductorSet_eq_conductor _ (conductor_mem_conductorSet _))
+
+lemma reduction_one (hn : n ≠ 0) :
+  (1 : DirichletCharacter R n).reduction = 1 :=
+by
+  rw [eq_one_iff_conductor_eq_one _]
+  { rw [(isPrimitive_def _).1 (1 : DirichletCharacter R n).reduction_isPrimitive, conductor_one hn] }
+  { rw [conductor_one hn]
+    apply Nat.one_ne_zero }
+
+/-lemma asso_DirichletCharacter_mul (ψ : DirichletCharacter R n) :
+  asso_DirichletCharacter (χ * ψ) = (asso_DirichletCharacter χ) * (asso_DirichletCharacter ψ) :=
+by
+  ext,
+  simp only [MonoidHom.mul_apply],
+  by_cases is_unit x,
+  { repeat { rw asso_DirichletCharacter_eq_char' _ h, },
+    simp only [MonoidHom.mul_apply, units.coe_mul], },
+  { repeat { rw asso_DirichletCharacter_eq_zero _ h, }, rw zero_mul, },
+end-/
+
+-- `mul_eq_asso_pri_char` changed to `asso_primitive_conductor_eq`
+lemma asso_primitive_conductor_eq {n : ℕ} (χ : DirichletCharacter R n) :
+  χ.reduction.conductor = χ.conductor := (isPrimitive_def χ.reduction).1 (reduction_isPrimitive χ)
+
+/-- Primitive character associated to multiplication of Dirichlet characters,
+  after changing both levels to the same -/
+noncomputable def mul {m : ℕ} (χ₁ : DirichletCharacter R n) (χ₂ : DirichletCharacter R m) :=
+reduction (changeLevel (Nat.dvd_lcm_left n m) χ₁ * changeLevel (Nat.dvd_lcm_right n m) χ₂)
+
+lemma mul_def {n m : ℕ} {χ : DirichletCharacter R n} {ψ : DirichletCharacter R m} :
+  χ.mul ψ = (changeLevel (Nat.dvd_lcm_left n m) χ * changeLevel (Nat.dvd_lcm_right n m) ψ).reduction := rfl
+
+namespace isPrimitive
+lemma mul {m : ℕ} (ψ : DirichletCharacter R m) : (mul χ ψ).isPrimitive := reduction_isPrimitive _
+end isPrimitive
+
+--/-- Composition of a Dirichlet character with a multiplicative homomorphism of units. -/
+--abbreviation comp {S : Type*} [comm_monoid_with_zero S] (f : units R →* units S) : DirichletCharacter S n := f.comp χ
+
+variable {S : Type} [CommRing S] {m : ℕ} (ψ : DirichletCharacter S m)
+
+/-- A Dirichlet character is odd if its value at -1 is -1. -/
+def is_odd : Prop := ψ (-1) = -1
+
+/-- A Dirichlet character is even if its value at -1 is 1. -/
+def is_even : Prop := ψ (-1) = 1
+
+lemma is_odd_or_is_even [NoZeroDivisors S] : ψ.is_odd ∨ ψ.is_even :=
+by
+  suffices : (ψ (-1))^2 = 1
+  { conv_rhs at this => rw [←one_pow 2]
+    rw [←sub_eq_zero] at this
+    rw [sq_sub_sq, mul_eq_zero, sub_eq_zero, add_eq_zero_iff_eq_neg] at this
+    cases' this with this this
+    { left
+      rw [is_odd]
+      simp only [this] }
+    { right
+      rw [is_even]
+      simp only [this] } }
+  { rw [←map_pow _, neg_one_sq, map_one] }
+-- can conditions on S be relaxed? comm needed for sq_sub_sq, and no_divisors needed for mul_eq_zero
+
+lemma toUnitHom_odd_eval_neg_one (hψ : ψ.is_odd) : ψ.toUnitHom (-1) = -1 :=
+by
+  rw [is_odd] at hψ
+  rw [← Units.eq_iff, MulChar.coe_toUnitHom]
+  simp only [Units.val_neg, Units.val_one, IsUnit.neg_iff, isUnit_one, not_true, hψ]
+
+lemma toUnitHom_even_eval_neg_one (hψ : ψ.is_even) :
+  ψ.toUnitHom (-1) = 1 :=
+by
+  rw [is_even] at hψ
+  rw [← Units.eq_iff, MulChar.coe_toUnitHom]
+  simp only [Units.val_neg, Units.val_one, IsUnit.neg_iff, isUnit_one, not_true, hψ]
+
+lemma odd_eval_sub (x : ZMod m) (hψ : ψ.is_odd) : ψ (m - x) = -(ψ x) :=
+by
+  rw [is_odd] at hψ
+  simp only [CharP.cast_eq_zero, zero_sub, IsUnit.neg_iff]
+  rw [←neg_one_mul, map_mul]
+  simp [hψ]
+
+lemma eval_sub (x : ZMod m) (hψ : is_even ψ) : ψ (m - x) = ψ x :=
+by
+  rw [is_even] at hψ
+  simp only [CharP.cast_eq_zero, zero_sub, IsUnit.neg_iff]
+  rw [←neg_one_mul, map_mul]
+  simp [hψ]
+
+end DirichletCharacter
--- PadicLFunctions4/DirCharProp.lean
+++ PadicLFunctions4/DirCharProp.lean
@@ -8,6 +8,7 @@ import PadicLFunctions4.ZModProp
 import Mathlib.Analysis.NormedSpace.Basic
 import Mathlib.Topology.Algebra.Group.Compact
 import Mathlib.Topology.ContinuousFunction.Compact
+import PadicLFunctions4.Basic
 
 /-!
 # Dirichlet characters
@@ -32,27 +33,17 @@ lemma Continuous {R : Type} [CommMonoidWithZero R] [TopologicalSpace R]
 lemma toUnitHom_continuous {R : Type} [CommMonoidWithZero R] [TopologicalSpace R]
   {n : ℕ} (χ : DirichletCharacter R n) : _root_.Continuous χ.toUnitHom := continuous_of_discreteTopology
 
-example {n : Nat} [NeZero n] : CompactSpace (ZMod n) := inferInstance
-
-example {R : Type} [MonoidWithZero R] [NormedGroup R] : NormedAddCommGroup R := by library_search
-
-example {n : Nat} [NeZero n] [MonoidWithZero R] [NormedGroup R] (n : ℕ) : Norm C(ZMod n, R) := by refine
-  NonUnitalSeminormedRing.toNorm
-
 -- `normed_group` was changed to `normed_add_comm_group`
-lemma asso_DirichletCharacter_bounded {R : Type} [CommMonoidWithZero R]
+lemma Bounded {R : Type} [CommMonoidWithZero R]
   [NormedAddCommGroup R] {n : ℕ} [NeZero n] (χ : DirichletCharacter R n) : ∃ M : ℝ,
   ‖(⟨χ, Continuous χ⟩ : C(ZMod n, R))‖ < M :=
 by
-  refine ⟨(⨆ i : ZMod n, ‖asso_DirichletCharacter χ i‖) + 1, _⟩,
-  apply lt_of_le_of_lt _ (lt_add_one _),
-  { convert le_refl _,
-    rw continuous_map.norm_eq_supr_norm,
-    simp only [continuous_map.coe_mk], },
-  { apply_instance, },
+  refine' ⟨(⨆ i : ZMod n, ‖χ i‖) + 1, _⟩
+  apply lt_of_le_of_lt _ (lt_add_one _)
+  convert le_refl _
+  rw [ContinuousMap.norm_eq_iSup_norm, ContinuousMap.coe_mk]
 
-
-lemma asso_DirichletCharacter_zero_range {R : Type} [CommMonoidWithZero R] [NormedGroup R]
+lemma Zero_range {R : Type} [CommMonoidWithZero R] [Norm R]
   (χ : DirichletCharacter R 0) : (Set.range (λ (i : ZMod 0) => ‖χ i‖)) =
     {‖χ 0‖, ‖χ 1‖, ‖χ (-1)‖} :=
 by
@@ -85,107 +76,100 @@ by
         refine ⟨-1, rfl⟩ } } }
 
 
-lemma asso_DirichletCharacter_zero_range_fin {R : Type} [CommMonoidWithZero R] [NormedGroup R]
+lemma Zero_range_fin {R : Type} [CommMonoidWithZero R] [Norm R]
   (χ : DirichletCharacter R 0) : (Set.range (λ (i : ZMod 0) => ‖χ i‖)).Finite :=
 by
-  rw [asso_DirichletCharacter_zero_range]
+  rw [Zero_range]
   simp only [Set.finite_singleton, Set.Finite.insert]
 
-lemma asso_DirichletCharacter_range_fin {R : Type} [CommMonoidWithZero R] [NormedGroup R] {n : ℕ}
+lemma range_Finite {R : Type} [CommMonoidWithZero R] [Norm R] {n : ℕ}
   (χ : DirichletCharacter R n) :
   (Set.range (λ (i : ZMod n) => ‖χ i‖)).Finite :=
 by
   cases n -- big improvement over by_cases' n!
-  { apply asso_DirichletCharacter_zero_range_fin _ }
+  { apply Zero_range_fin _ }
   { exact Set.finite_range fun i => ‖χ i‖ }
 
-lemma asso_DirichletCharacter_range_BddAbove {R : Type} [CommMonoidWithZero R] [NormedGroup R]
+lemma BddAbove {R : Type} [CommMonoidWithZero R] [Norm R]
   {n : ℕ} (χ : DirichletCharacter R n) :
-  BddAbove (Set.range (λ (i : ZMod n) => ‖χ i‖)) :=
-Set.Finite.bddAbove (asso_DirichletCharacter_range_fin _)
+  BddAbove (Set.range (λ (i : ZMod n) => ‖χ i‖)) := Set.Finite.bddAbove (range_Finite _)
 
-lemma asso_DirichletCharacter_bounded_spec {R : Type} [CommMonoidWithZero R] [NormedGroup R]
+lemma Bounded_spec {R : Type} [CommMonoidWithZero R] [SeminormedAddGroup R]
   {n : ℕ} (χ : DirichletCharacter R n) :
   ∃ M : ℝ, (∀ a, ‖χ a‖ < M) ∧ 0 < M :=
 by
   refine' ⟨(⨆ i : ZMod n, ‖χ i‖) + 1, λ a => lt_of_le_of_lt _
     (lt_add_one _), (lt_add_of_le_of_pos _ _)⟩
-  { apply le_ciSup (asso_DirichletCharacter_range_BddAbove _) a }
+  { apply le_ciSup (BddAbove _) a }
   { apply le_ciSup_of_le _ 1 _
 --    swap 3
 --    { exact 1 }
-    { apply asso_DirichletCharacter_range_BddAbove }
+    { apply BddAbove }
     { apply norm_nonneg (χ 1) } }
   { norm_num }
 
 /-- Every Dirichlet character is bounded above. -/
 @[reducible]
-noncomputable def bound {R : Type} [CommMonoidWithZero R] [NormedGroup R] {n : ℕ}
+noncomputable def bound {R : Type} [CommMonoidWithZero R] [SeminormedAddGroup R] {n : ℕ}
   (χ : DirichletCharacter R n) : ℝ :=
-Classical.choose (DirichletCharacter.asso_DirichletCharacter_bounded_spec χ)
+Classical.choose (DirichletCharacter.Bounded_spec χ)
 
-lemma lt_bound {R : Type} [CommMonoidWithZero R] [NormedGroup R] {n : ℕ}
+lemma lt_bound {R : Type} [CommMonoidWithZero R] [SeminormedAddGroup R] {n : ℕ}
   (χ : DirichletCharacter R n) (a : ZMod n) :
   ‖χ a‖ < DirichletCharacter.bound χ :=
-(Classical.choose_spec (DirichletCharacter.asso_DirichletCharacter_bounded_spec χ)).1 a
+(Classical.choose_spec (DirichletCharacter.Bounded_spec χ)).1 a
 
-lemma bound_pos {R : Type} [CommMonoidWithZero R] [NormedGroup R] {n : ℕ}
+lemma bound_pos {R : Type} [CommMonoidWithZero R] [SeminormedAddGroup R] {n : ℕ}
   (χ : DirichletCharacter R n) : 0 < DirichletCharacter.bound χ :=
-(Classical.choose_spec (DirichletCharacter.asso_DirichletCharacter_bounded_spec χ)).2
+(Classical.choose_spec (DirichletCharacter.Bounded_spec χ)).2
 
-open ZMod
+open ZMod FactorsThrough
 lemma mul_eval_of_coprime {R : Type} [CommMonoidWithZero R] {n m : ℕ}
   (χ : DirichletCharacter R n) (ψ : DirichletCharacter R m) {a : ℕ} (ha : a.Coprime (n * m)) :
-  (DirichletCharacter.mul χ ψ) a = χ a * (ψ a) :=
+  (mul χ ψ) a = χ a * (ψ a) :=
 by
-  rw [mul, ←(ZMod.cast_nat_cast (conductor.dvd_lev (change_level (dvd_lcm_left n m) χ *
-    change_level (dvd_lcm_right n m) ψ)) a)],
-  { have dvd : lcm n m ∣ n * m := lcm_dvd_iff.2 ⟨(dvd_mul_right _ _), (dvd_mul_left _ _)⟩,
-    have := ZMod.isUnit_of_is_coprime_dvd dvd ha,
-    rw ←change_level.asso_DirichletCharacter_eq' _ (conductor.dvd_lev _) this,
-    delta reduction,
-    rw [←(Factors_through.spec _ (conductor.Factors_through (change_level _ χ * change_level _ ψ))),
-      asso_DirichletCharacter_mul, Monoid_hom.mul_apply, change_level.asso_DirichletCharacter_eq'
-      _ _ this, change_level.asso_DirichletCharacter_eq' _ _ this, ZMod.cast_nat_cast
-      (dvd_lcm_left n m), ZMod.cast_nat_cast (dvd_lcm_right n m)],
-    any_goals { refine ZMod.char_p _, }, },
-  { refine ZMod.char_p _, },
+  rw [mul_def, ←(ZMod.cast_nat_cast (conductor_dvd_level (changeLevel (dvd_lcm_left n m) χ *
+    changeLevel (dvd_lcm_right n m) ψ)) a)]
+  { have dvd : lcm n m ∣ n * m := lcm_dvd_iff.2 ⟨(dvd_mul_right _ _), (dvd_mul_left _ _)⟩
+    --have := ZMod.isUnit_of_is_coprime_dvd dvd ha
+    rw [← eq_changeLevel _ (conductor_dvd_level _)]--←changeLevel.asso_DirichletCharacter_eq' _ (conductor_dvd_level _) _]
+    delta reduction
+    rw [←(Factors_through.spec _ (conductor.Factors_through (changeLevel _ χ * changeLevel _ ψ))),
+      asso_DirichletCharacter_mul, Monoid_hom.mul_apply, changeLevel.asso_DirichletCharacter_eq'
+      _ _ this, changeLevel.asso_DirichletCharacter_eq' _ _ this, ZMod.cast_nat_cast
+      (dvd_lcm_left n m), ZMod.cast_nat_cast (dvd_lcm_right n m)]
+    any_goals { refine' ZMod.char_p _ } }
+  { refine ZMod.char_p _ }
 
-
-namespace asso_DirichletCharacter
 lemma eval_mul_sub {R : Type} [CommMonoidWithZero R] {n : ℕ} (χ : DirichletCharacter R n)
-  (k x : ℕ) : asso_DirichletCharacter χ (k * n - x) = asso_DirichletCharacter χ (-1) *
-  (asso_DirichletCharacter χ x) :=
-by { rw [ZMod.nat_cast_self, mul_zero, zero_sub, neg_eq_neg_one_mul, Monoid_hom.map_mul], }
+  (k x : ℕ) : χ (k * n - x) = χ (-1) * χ x :=
+by { rw [ZMod.nat_cast_self, mul_zero, zero_sub, neg_eq_neg_one_mul, map_mul] }
 
 lemma eval_mul_sub' {R : Type} [CommMonoidWithZero R] {n k : ℕ} (χ : DirichletCharacter R n)
-  (hk : n ∣ k) (x : ℕ) : χ (k - x) = χ (-1) * (χ x) :=
+  (hk : n ∣ k) (x : ℕ) : χ (k - x) = χ (-1) * χ x :=
 by
   have : (k : ZMod n) = 0
   { rw [←ZMod.nat_cast_mod, Nat.mod_eq_zero_of_dvd hk, Nat.cast_zero] }
   rw [this, zero_sub, neg_eq_neg_one_mul, map_mul]
 
 --`asso_DirichletCharacter_equiv` changed to `asso_DirichletCharacter.reduction`
-lemma reduction {S : Type} [CommMonoidWithZero S] {m : ℕ}
-  (ψ : DirichletCharacter S m) (h : is_primitive ψ) (a : ℕ) :
-  ψ.reduction a = ψ a :=
-by
-  by_cases h' : isUnit (a : ZMod m)
-  { conv_rhs =>
-      rw [Factors_through.spec ψ (conductor.Factors_through ψ)]
-    rw [change_level.asso_DirichletCharacter_eq' _ _ h']
+-- note that these two cannot be equal since their levels are not the same, according to Lean
+lemma reduction_def {S : Type} [CommMonoidWithZero S] {m : ℕ}
+  (ψ : DirichletCharacter S m) (h : isPrimitive ψ) (a : ℕ) :
+  ψ.reduction a = ψ a := by
+  by_cases h' : IsUnit (a : ZMod m)
+  { conv_rhs => rw [eq_changeLevel ψ (factorsThrough_conductor ψ)]
+    rw [reduction]
+    rw [eq_changeLevel _ _]
     apply congr
     { congr }
     { rw [ZMod.cast_nat_cast _]
-      swap
-      { refine' ZMod.char_p _ }
+      --swap, { refine zmod.char_p _, },
       { apply conductor.dvd_lev _ } } }
-  { repeat { rw [asso_DirichletCharacter_eq_zero] }
+  { repeat { rw [asso_dirichlet_character_eq_zero] }
     { assumption }
-    rw [(is_primitive_def _).1 h]
+    rw [(isPrimitive_def _).1 h]
     apply h' }
-
-end asso_DirichletCharacter
 
 /-- The level at which the Dirichlet character is defined. -/
 --@[nolint unused_arguments] -- this was used to remove linter error
@@ -193,49 +177,47 @@ def lev {R : Type} [CommMonoidWithZero R] {n : ℕ} (χ : DirichletCharacter R n
 -- dont know how to remove this linting error
 
 lemma lev_mul_dvd_lcm {R : Type} [CommMonoidWithZero R] {n k : ℕ} (χ : DirichletCharacter R n)
-  (ψ : DirichletCharacter R k) : lev (mul χ ψ) ∣ lcm n k := dvd_trans (conductor.dvd_lev _) dvd_rfl
+  (ψ : DirichletCharacter R k) : lev (mul χ ψ) ∣ lcm n k := dvd_trans (conductor_dvd_level _) dvd_rfl
 
 lemma lev_mul_dvd_mul_lev {R : Type} [CommMonoidWithZero R] {n k : ℕ} (χ : DirichletCharacter R n)
   (ψ : DirichletCharacter R k) : lev (mul χ ψ) ∣ n * k :=
-dvd_trans (conductor.dvd_lev _) (nat.lcm_dvd_mul _ _)
+dvd_trans (conductor_dvd_level _) (Nat.lcm_dvd_mul _ _)
 
 open DirichletCharacter
-lemma mul_eval_neg_one {R : Type} [CommMonoidWithZero R] {n m : ℕ} [NeZero n] [Fact (0 < m)]
+lemma mul_eval_neg_one {R : Type} [CommMonoidWithZero R] {n m : ℕ} [h1 : NeZero n] [h2 : NeZero m]
   (χ : DirichletCharacter R n) (ψ : DirichletCharacter R m) :
   (DirichletCharacter.mul χ ψ) (-1 : ℤ) =
   χ (-1) * ψ (-1) :=
 by
-  have one_le : 1 ≤ n * m := Nat.succ_le_iff.2 (Nat.mul_pos (Fact.out _) (Fact.out _)),
-  have f1 : (-1 : ZMod (lev (χ.mul ψ))) = ↑((n * m - 1) : ℕ),
-  { rw [Nat.cast_sub one_le, (ZMod.nat_coe_ZMod_eq_zero_iff_dvd _ _).2 (dvd_trans (conductor.dvd_lev _)
-      (lcm_dvd (dvd_mul_right _ _) (dvd_mul_left _ _))), zero_sub, Nat.cast_one], },
-  rw [int.cast_neg, int.cast_one, f1,
-    mul_eval_of_coprime _ _ (Nat.coprime_sub (Nat.coprime_one_right _) one_le)],
+  have one_le : 1 ≤ n * m := Nat.succ_le_iff.2 (Nat.mul_pos (Nat.pos_of_ne_zero (NeZero.ne _)) (Nat.pos_of_ne_zero (NeZero.ne _)))
+  have f1 : (-1 : ZMod (lev (χ.mul ψ))) = ↑((n * m - 1) : ℕ)
+  { rw [Nat.cast_sub one_le, (ZMod.nat_cast_zmod_eq_zero_iff_dvd _ _).2 (dvd_trans (conductor_dvd_level _)
+      (lcm_dvd (dvd_mul_right _ _) (dvd_mul_left _ _))), zero_sub, Nat.cast_one] }
+  rw [Int.cast_neg, Int.cast_one, f1,
+    mul_eval_of_coprime _ _ _]
   simp only [Nat.cast_sub one_le, Nat.cast_sub one_le, Nat.cast_mul, ZMod.nat_cast_self, zero_mul,
-    Nat.cast_one, zero_sub, mul_zero],
+    Nat.cast_one, zero_sub, mul_zero]
+  aesop
 
-
-lemma mul_eval_int {R : Type} [comm_CommMonoidWithZero R] {n m : ℕ} [NeZero n] [Fact (0 < m)]
+lemma mul_eval_int {R : Type} [CommMonoidWithZero R] {n m : ℕ} [NeZero n] [NeZero m]
   (χ : DirichletCharacter R n) (ψ : DirichletCharacter R m) {a : ℤ}
-  (ha : is_coprime a (n * m : ℤ)) : asso_DirichletCharacter (DirichletCharacter.mul χ ψ) a =
-  asso_DirichletCharacter χ a * asso_DirichletCharacter ψ a :=
+  (ha : IsCoprime a (n * m : ℤ)) : (DirichletCharacter.mul χ ψ) a = χ a * ψ a :=
 by
-  cases' a,
-  { change asso_DirichletCharacter (DirichletCharacter.mul χ ψ) a =
-      asso_DirichletCharacter χ a * asso_DirichletCharacter ψ a,
-    rw mul_eval_of_coprime χ ψ (Nat.is_coprime_iff_coprime.1 ha), },
-  { rw [int.neg_succ_of_nat_coe, ←neg_one_mul, int.cast_mul, Monoid_hom.map_mul, mul_eval_neg_one],
-    rw [int.neg_succ_of_nat_coe, is_coprime.neg_left_iff] at ha,
-    rw [int.cast_coe_nat, mul_eval_of_coprime χ ψ (Nat.is_coprime_iff_coprime.1 ha),
-      mul_mul_mul_comm],
-    simp_rw [←Monoid_hom.map_mul, int.cast_mul],
-    norm_cast, },
+  cases' a with a a
+  { simp only [Int.ofNat_eq_coe, Int.cast_ofNat]
+    rw [mul_eval_of_coprime χ ψ (Nat.isCoprime_iff_coprime.1 ha)] }
+  { rw [Int.negSucc_eq, IsCoprime.neg_left_iff] at ha
+    rw [Int.negSucc_coe, ←neg_one_mul, Int.cast_mul, map_mul, mul_eval_neg_one,
+      Int.cast_ofNat _, mul_eval_of_coprime χ ψ (Nat.isCoprime_iff_coprime.1 ha),
+      mul_mul_mul_comm]
+    simp_rw [←map_mul, Int.cast_mul]
+    norm_cast }
 
-
-instance {R : Type} [CommMonoidWithZero R] {n : ℕ} : Pow (DirichletCharacter R n) ℕ :=
+--not sure if these are needed
+/-instance {R : Type} [CommMonoidWithZero R] {n : ℕ} : Pow (DirichletCharacter R n) ℕ :=
 Monoid.Pow
 
 lemma pow_apply {R : Type} [CommMonoidWithZero R] {n : ℕ} (k : ℕ)
   (χ : DirichletCharacter R n) (a : (ZMod n)ˣ) :
-  ((χ.toUnitHom : MonoidHom (Units (ZMod n)) (Units R))^k) a = (χ a)^k := rfl
- DirichletCharacter
+  ((χ.toUnitHom : MonoidHom (Units (ZMod n)) (Units R))^k) a = (χ a)^k := rfl-/
+end DirichletCharacter
